e925b347427d4aa4e61f9b86e36572ae832a1283
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-13 02:36:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+  static titleize(string) {
+    let nonCapitalizeWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let newString = string.split(' ').map(word => nonCapitalizeWords.includes(word) ? word : this.capitalize(word)).join(' ')
+    return this.capitalize(newString)
+  }
 }
