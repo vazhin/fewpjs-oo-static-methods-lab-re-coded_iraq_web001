9b1984aa14851e33f3cda6aee31be4d9ebacbe92
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-13 01:58:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ class Formatter {
   static capitalize(string){
     return string.charAt(0).toUpperCase() + string.slice(1)
   }
+  static sanitize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
 }
